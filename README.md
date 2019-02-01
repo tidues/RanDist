56d9dede9eb3b0c463a3b444818338a5b1a8f05e
tidues <tidues@gmail.com>
2019-02-01 14:22:23 -0500

    readme v0.1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Distance between Random Events
+
+This package is for symbolically and numerically calculating the arbitrary order moments, pdf, cdf and their conditional counterparts of the distance between two random events in a given graph.
+
+The details will be added soon.
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Distance statistics for two random events on a network",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/tidues/Distance-Distribution-of-Random-Events",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
